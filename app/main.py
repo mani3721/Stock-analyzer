d030e56dc00a1c7de25a536c5b79fe1d8fd5a6bf
mani3721 <petchimuthuvoc@gmail.com>
2026-06-21 16:38:11 +0530

    fix corss issue

--- app/main.py
+++ app/main.py
@@ -16,8 +16,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["GET"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
+    allow_credentials=False,
 )
 
 app.include_router(analysis_router)
